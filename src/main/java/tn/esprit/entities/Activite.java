ab5dbc39292e35ce08fc9761da2a28c7474cc64b
rimeh-git <chagour.rimeh@esprit.tn>
2026-02-16 12:49:26 +0100

    Update Activite.java

--- src/main/java/tn/esprit/entities/Activite.java
+++ src/main/java/tn/esprit/entities/Activite.java
@@ -14,6 +14,7 @@ public class Activite {
 
     public Activite() {}
 
+    // 🔹 Constructeur principal complet
     public Activite(String imageUrl, String bonneReponse,
                     int score, int duree,
                     String resultat, int jeuId,
@@ -29,23 +30,32 @@ public class Activite {
         this.typeActivite = typeActivite;
     }
 
+    // 🔹 Constructeur avec ID
     public Activite(int id, String imageUrl, String bonneReponse,
-                    int score, int duree, String resultat,
-                    int jeuId, String question, String typeActivite) {
+                    int score, int duree,
+                    String resultat, int jeuId,
+                    String question, String typeActivite) {
 
+        this(imageUrl, bonneReponse, score, duree, resultat, jeuId, question, typeActivite);
         this.id = id;
-        this.imageUrl = imageUrl;
-        this.bonneReponse = bonneReponse;
-        this.score = score;
-        this.duree = duree;
-        this.resultat = resultat;
-        this.jeuId = jeuId;
-        this.question = question;
-        this.typeActivite = typeActivite;
     }
 
+    // 🔹 Constructeur simplifié (pour anciens appels)
+    public Activite(String imageUrl, String bonneReponse,
+                    int score, int duree,
+                    String resultat, int jeuId) {
 
+        this(imageUrl, bonneReponse, score, duree, resultat, jeuId, null, null);
+    }
 
+    // 🔹 Constructeur ultra simple (pour test Main)
+    public Activite(String question, int score,
+                    String typeActivite, int jeuId) {
+
+        this(null, null, score, 0, null, jeuId, question, typeActivite);
+    }
+
+    // Getters
     public int getId() { return id; }
     public String getImageUrl() { return imageUrl; }
     public String getBonneReponse() { return bonneReponse; }
@@ -55,60 +65,10 @@ public class Activite {
     public int getJeuId() { return jeuId; }
     public String getQuestion() { return question; }
     public String getTypeActivite() { return typeActivite; }
-    public Activite(String imageUrl, String bonneReponse,
-                    int score, int duree, String resultat,
-                    int jeuId) {
 
-        this.imageUrl = imageUrl;
-        this.bonneReponse = bonneReponse;
-        this.score = score;
-        this.duree = duree;
-        this.resultat = resultat;
-        this.jeuId = jeuId;
-    }
-
-
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public void setTypeActivite(String typeActivite) {
-        this.typeActivite = typeActivite;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public void setBonneReponse(String bonneReponse) {
-        this.bonneReponse = bonneReponse;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public void setDuree(int duree) {
-        this.duree = duree;
-    }
-
-    public void setResultat(String resultat) {
-        this.resultat = resultat;
-    }
-
-    public void setJeuId(int jeuId) {
-        this.jeuId = jeuId;
-    }
-
-    @Override
-    public String toString() {
-        return "Activite{" +
-                "id=" + id +
-                ", question='" + question + '\'' +
-                ", bonneReponse='" + bonneReponse + '\'' +
-                ", score=" + score +
-                ", jeuId=" + jeuId +
-                '}';
-    }
+    // Setters
+    public void setQuestion(String question) { this.question = question; }
+    public void setScore(int score) { this.score = score; }
+    public void setTypeActivite(String typeActivite) { this.typeActivite = typeActivite; }
+    public void setJeuId(int jeuId) { this.jeuId = jeuId; }
 }
